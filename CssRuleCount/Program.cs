4f7f7bb81a2e7a2d77734e1d3f1452c641ab273c
Matt Higgins <mhhiggins@gmail.com>
2011-08-24 00:09:23 -0400

    initial commit

---
+++ CssRuleCount/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CssRuleCount
+{
+    public class Program
+    {
+        public const string HarnessTemplate = @"
+<html>
+    <head>
+        <link rel=""stylesheet"" type=""text/css"" href=""{0}"" />
+    </head>
+</html>";
+
+        public const int RulesLimit = 4095;
+        public static bool RuleLimitExceeded { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (String.IsNullOrWhiteSpace(arg))
+                    continue;
+
+                if (!File.Exists(arg))
+                {
+                    Console.Error.WriteLine("Skipping unrecognized file: {0}", arg);
+                    continue;
+                }
+
+                LaunchBrowser(new Uri(Path.GetFullPath(arg)).AbsoluteUri);
+            }
+
+            System.Environment.Exit(RuleLimitExceeded ? 1 : 0);
+        }
+
+        private static void LaunchBrowser(string stylesheet)
+        {
+            Thread t = new Thread(() =>
+            {
+                WebBrowser wb = new WebBrowser();
+
+                wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser_DocumentCompleted);
+                wb.ScriptErrorsSuppressed = true;
+                wb.DocumentText = String.Format(HarnessTemplate, stylesheet);
+
+                Application.Run();
+            });
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+        }
+
+        private static void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = sender as WebBrowser;
+
+            if (wb != null)
+            {
+                dynamic dom = wb.Document.DomDocument;
+                string stylesheet = dom.styleSheets[0].href;
+                int rules = dom.styleSheets[0].rules.length;
+
+                string file = Path.GetFileName(new Uri(stylesheet).LocalPath);
+
+                Console.WriteLine("Stylesheet \"{0}\" contains {1} rule{2}.", file, rules, rules != 1 ? "s" : "");
+
+                if (rules > RulesLimit)
+                    RuleLimitExceeded = true;
+            }
+
+            Application.ExitThread();
+        }
+    }
+}
